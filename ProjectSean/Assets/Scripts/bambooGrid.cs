7e4d93a96a010bd3b42046a814795207b4e6302f
Amet DeCrypt <ametueraspirant@yahoo.com>
2018-04-03 05:28:47 -0600

    boufed the island
    with a ray

---
+++ ProjectSean/Assets/Scripts/bambooGrid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bambooGrid : MonoBehaviour {
+
+	public isBamboo[] isBamboo;
+
+	int[,] bamboo;
+    [SerializeField]
+	int gridSizeX = 16;
+    [SerializeField]
+	int gridSizeY = 7;
+
+	void Start () {
+		bamboo = new int[gridSizeX, gridSizeY];
+
+		for (int X = 0; X <= gridSizeX; X++) {
+			for (int Y = 0; Y <= gridSizeY; Y++) {
+				bamboo [X, Y] = 0;
+
+			}
+        }
+	}
+    void generateMap()
+	{
+		for (int X = 0; X <= gridSizeX; X++)
+		{
+			for (int Y = 0; Y <= gridSizeY; Y++)
+			{
+
+			}
+		}
+	}
+
+}
