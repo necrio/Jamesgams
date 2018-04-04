c512720f7b57564621869dbf03ab98fd151206e7
Amet DeCrypt <ametueraspirant@yahoo.com>
2018-04-04 13:35:54 -0600

    projection is projecting
    fuckyea

---
+++ ProjectSean/Assets/Scripts/TileMap.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour
+{
+
+    public TileType[] tileTypes;
+    int[,] tiles;
+    int mapSizeX = 16;
+    int mapSizeZ = 7;
+
+    void Start ()
+{
+        tiles = new int[mapSizeX, mapSizeZ];
+        for (int x=0; x < mapSizeX; x++)
+        {
+            for (int z = 0; z < mapSizeZ; z++)
+            {
+                tiles[x, z] = 0;
+
+            }
+
+        }
+        tiles[0, 0] = 1;
+        tiles[0, 1] = 1;
+        tiles[2, 2] = 1;
+        tiles[2, 5] = 1;
+        tiles[2, 6] = 1;
+        tiles[3, 0] = 1;
+        tiles[3, 6] = 1;
+        tiles[4, 0] = 1;
+        tiles[4, 1] = 1;
+        tiles[4, 3] = 1;
+        tiles[5, 0] = 1;
+        tiles[5, 4] = 1;
+        tiles[5, 6] = 1;
+
+        GenerateMap();
+
+	}	
+
+    void GenerateMap()
+    {
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int z = 0; z < mapSizeZ; z++)
+            {
+                TileType tt = tileTypes[tiles[x, z]];
+                Instantiate(tt.tilePrefab, new Vector3(x, 0, z), Quaternion.identity);
+
+            }
+
+        }
+
+    }
+
+}
