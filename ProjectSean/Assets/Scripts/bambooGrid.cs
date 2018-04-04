9d962cd4c5f535c9017753c61a6617a37b840b5f
Amet DeCrypt <ametueraspirant@yahoo.com>
2018-04-04 05:48:46 -0600

    array changes
    still bugged

--- ProjectSean/Assets/Scripts/bambooGrid.cs
+++ ProjectSean/Assets/Scripts/bambooGrid.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class bambooGrid : MonoBehaviour {
 
-	public IsBamboo[] dooBaDee;
+	public IsBamboo[] gridType;
 
 	int[,] bamboo;
     [SerializeField]
@@ -15,8 +15,8 @@ public class bambooGrid : MonoBehaviour {
 	void Start () {
 		bamboo = new int[gridSizeX, gridSizeY];
 
-		for (int X = 0; X <= gridSizeX; X++) {
-			for (int Y = 0; Y <= gridSizeY; Y++) {
+		for (int X = 0; X < gridSizeX; X++) {
+			for (int Y = 0; Y < gridSizeY; Y++) {
 				bamboo [X, Y] = 0;
 
 			}
@@ -27,20 +27,19 @@ public class bambooGrid : MonoBehaviour {
 		bamboo[3,4]=1;
 		bamboo[4,5]=1;
 		bamboo[5,6]=1;
-		bamboo[6,7]=1;
-		bamboo[7,8]=1;
+		bamboo[6,6]=1;
+		bamboo[7,6]=1;
 
 		generateMap();
 	}
     void generateMap()
 	{
-		for (int X = 0; X <= gridSizeX; X++)
+		for (int X = 0; X < gridSizeX; X++)
 		{
-			for (int Y = 0; Y <= gridSizeY; Y++)
+			for (int Y = 0; Y < gridSizeY; Y++)
 			{
-				IsBamboo tt = dooBaDee[bamboo[X, Y]];
-
-				Instantiate(tt.bambooVisualPrefab, new Vector3(X, Y, 0), Quaternion.identity);
+				Instantiate(gridType[bamboo[X, Y]], new Vector3(X, Y, 0), Quaternion.identity);
+				Debug.Log("debug");
 
 			}
 		}
